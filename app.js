a585f7d40615fff6eca02abc1112e0c5e7628382
Yaasir Cheekoory <yaasir1997@gmail.com>
2023-03-21 23:52:35 +0400

    working on logic

---
+++ app.js
@@ -0,0 +1,18 @@
+const searchbarElement = document.querySelector(".searchbar");
+const teamsElement = document.querySelector(".teams");
+const teamElement = document.querySelectorAll(".team");
+
+
+searchbarElement.addEventListener("keyup", () => {
+  let searchbarValue = document.querySelector(".searchbar").value;
+  searchbarValue = searchbarValue.toLowerCase();
+
+  for (let i = 0; i < teamElement.length; i++) {
+    let team = teamElement[i].innerHTML.toLowerCase();
+
+    if (team.includes(searchbarValue)) {
+      teamsElement.innerHTML = `<li class="team">${teamElement[i]}</li>`;
+    }
+  }
+
+});
